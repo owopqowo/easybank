1518bc7db65c57d507fd00bf2cacf95b83a997b7
owopqowo <owo9370@gmail.com>
2022-11-23 17:57:17 +0900

    add animated hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,14 @@
-console.log('hello');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const btnHamburger = document.querySelector('#btnHamburger');
+btnHamburger.addEventListener('click', function (e) {
+  if (header.classList.contains('open')) {
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
+    header.classList.remove('open');
+  } else {
+    header.classList.add('open');
+    overlay.classList.remove('fade-out');
+    overlay.classList.add('fade-in');
+  }
+});
