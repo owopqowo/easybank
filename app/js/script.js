2eab261de49c4b374712cd4fe779e152d0ae59ad
owopqowo <owo9370@gmail.com>
2022-11-24 20:55:53 +0900

    add mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,14 @@
 const header = document.querySelector('.header');
 const btnHamburger = document.querySelector('#btnHamburger');
+const links = document.querySelector('.header__links');
+const linksClone = links.cloneNode(true);
+
+linksClone.className = '';
+linksClone.classList.add('header__menu', 'has-fade', 'hide-for-desktop');
+header.append(linksClone);
+
 const fadeElems = document.querySelectorAll('.has-fade');
+
 btnHamburger.addEventListener('click', function (e) {
   if (header.classList.contains('open')) {
     fadeElems.forEach(function (elem) {
