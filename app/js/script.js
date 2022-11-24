da435d74cf054dfdff69ea499efc9b87a2484839
owopqowo <owo9370@gmail.com>
2022-11-24 17:58:12 +0900

    fix header

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,18 @@
 const header = document.querySelector('.header');
-const overlay = document.querySelector('.overlay');
 const btnHamburger = document.querySelector('#btnHamburger');
+const fadeElems = document.querySelectorAll('.has-fade');
 btnHamburger.addEventListener('click', function (e) {
   if (header.classList.contains('open')) {
-    overlay.classList.remove('fade-in');
-    overlay.classList.add('fade-out');
+    fadeElems.forEach(function (elem) {
+      elem.classList.remove('fade-in');
+      elem.classList.add('fade-out');
+    });
     header.classList.remove('open');
   } else {
     header.classList.add('open');
-    overlay.classList.remove('fade-out');
-    overlay.classList.add('fade-in');
+    fadeElems.forEach(function (elem) {
+      elem.classList.remove('fade-out');
+      elem.classList.add('fade-in');
+    });
   }
 });
