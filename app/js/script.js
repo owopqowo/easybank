70c142889ed2ec352f80fb0fc3e4bcd19b5eb9c0
owopqowo <owo9370@gmail.com>
2022-11-29 15:54:10 +0900

    fix mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,6 @@
 const header = document.querySelector('.header');
 const btnHamburger = document.querySelector('#btnHamburger');
+const overlay = document.querySelector('.overlay');
 const links = document.querySelector('.header__links');
 const linksClone = links.cloneNode(true);
 
@@ -9,18 +10,29 @@ header.append(linksClone);
 
 const fadeElems = document.querySelectorAll('.has-fade');
 
-btnHamburger.addEventListener('click', function (e) {
+btnHamburger.addEventListener('click', function () {
   if (header.classList.contains('open')) {
     fadeElems.forEach(function (elem) {
       elem.classList.remove('fade-in');
       elem.classList.add('fade-out');
     });
     header.classList.remove('open');
+    document.body.classList.remove('scroll-hidden');
   } else {
     header.classList.add('open');
     fadeElems.forEach(function (elem) {
       elem.classList.remove('fade-out');
       elem.classList.add('fade-in');
     });
+    document.body.classList.add('scroll-hidden');
   }
+});
+
+overlay.addEventListener('click', function () {
+  fadeElems.forEach(function (elem) {
+    elem.classList.remove('fade-in');
+    elem.classList.add('fade-out');
+  });
+  header.classList.remove('open');
+  document.body.classList.remove('scroll-hidden');
 });
